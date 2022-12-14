deb6e85b1660b5211e8b737e792d672f3d1e6a60
shivam2320 <alekarshivam23@gmail.com>
2022-12-14 12:04:16 +0530

    updates

--- src/SweepDust.sol
+++ src/SweepDust.sol
@@ -7,13 +7,16 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./UniswapV3.sol";
 
-//Transfer multiple tokens to this contract
-//Swap those tokens for required/stable/native token
+/// @title SweepDust
+/// @notice Allows users to swap small balance tokens into a single token
+
 contract SweepDust is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
+    /// @notice UniswapV3 implementation address
     UniV3Provider public dex;
 
+    /// @notice Events
     event sweep(address[] tokensList, uint256[] amount, address requiredToken);
 
     constructor(address _dex) {
@@ -21,15 +24,16 @@ contract SweepDust is Ownable, ReentrancyGuard {
     }
 
     /// @notice This function is responsible for swapping multiple tokens into a single token
-    /// @dev Explain to a developer any extra details
+    /// @dev This contract uses UniswapV3 for swapping
     /// @param tokensList List of ERC20 tokenAddresses to swap
     /// @param amount Amount of tokens to swap
-    /// @param requiredToken
+    /// @param requiredToken Address of required ERC20 token
     function sweepDust(
         address[] memory tokensList,
         uint256[] memory amount,
         address requiredToken
     ) external nonReentrant {
+        require(requiredToken != address(0), "");
         _swapForERC20(tokensList, amount, requiredToken);
         emit sweep(tokensList, amount, requiredToken);
     }
